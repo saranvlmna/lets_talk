c396b8aceb37966b152e753128b046f8e3bde503
saranjith <saranjith@metrictreelabs.com>
2022-12-02 21:33:24 +0530

    chat gateway setup

---
+++ src/chatgateway.ts
@@ -0,0 +1,35 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { ChatService } from './chat/chat.service';
+@WebSocketGateway()
+export class ChatGateway {
+  constructor(private chatService: ChatService) {}
+  @WebSocketServer()
+  async handleConnection(socket: Socket, ...args: any[]) {
+    return this.chatService.activeUser(
+      socket.handshake.query.userId,
+      socket.id,
+    );
+  }
+  
+  handleDisconnect(socket: Socket) {
+    return this.chatService.offlineUser(socket.id);
+  }
+
+  @SubscribeMessage('sendMessage')
+  async handleMessage(
+    @MessageBody() message: any,
+    @ConnectedSocket() socket: Socket,
+  ): Promise<void> {
+    let user = await this.chatService.findUser(message['reciverId']);
+    if (user) {
+      socket.to(user.socketId).emit('reciveMessage', message);
+    }
+  }
+}
